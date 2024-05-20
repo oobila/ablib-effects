f6bd330a976f63d9cbe8e36c84eb9262fce16277
BuildTools <unconfigured@null.spigotmc.org>
2024-05-20 16:28:55 +0100

    updating AttributeManager for newer common lib

--- src/main/java/com/github/oobila/bukkit/effects/AttributeManager.java
+++ src/main/java/com/github/oobila/bukkit/effects/AttributeManager.java
@@ -1,17 +1,22 @@
 package com.github.oobila.bukkit.effects;
 
-import com.github.oobila.bukkit.common.ABCommon;
-import com.github.oobila.bukkit.common.CannotFindABCoreException;
-import com.github.oobila.bukkit.common.ServerContextException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 
 import static com.github.oobila.bukkit.common.ABCommon.log;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AttributeManager {
+
+    private static final Map<Class<? extends Attribute>, Map<String, Attribute>> ATTRIBUTE_MAP = new HashMap<>();
 
     public static Attribute register(Attribute attribute) {
         return register(attribute, Attribute.class);
@@ -23,51 +28,45 @@ public class AttributeManager {
     }
 
     private static Attribute register(Attribute attribute, Class<? extends Attribute> type) {
-        try {
-            return ABCommon.store(type, attribute.getName(), attribute);
-        } catch (CannotFindABCoreException e) {
-            log(Level.SEVERE, e.getMessage());
-        } catch (ServerContextException e) {
+        ATTRIBUTE_MAP.putIfAbsent(type, new HashMap<>());
+        if (ATTRIBUTE_MAP.get(type).containsKey(attribute.getName())) {
             log(Level.WARNING, "{0} [{1}] already exists", type.getSimpleName(), attribute.getName());
+            return null;
         }
-        return null;
+        return ATTRIBUTE_MAP.get(type).put(attribute.getName(), attribute);
+    }
+
+    private static Collection<Attribute> getAttributes(Class<? extends Attribute> type) {
+        if (ATTRIBUTE_MAP.containsKey(type)) {
+            return ATTRIBUTE_MAP.get(type).values();
+        } else {
+            return Collections.emptySet();
+        }
+    }
+
+    private static Collection<Attribute> getAttributes() {
+        List<Attribute> allAttributes = new ArrayList<>();
+        ATTRIBUTE_MAP.keySet().forEach(key ->
+            allAttributes.addAll(ATTRIBUTE_MAP.get(key).values())
+        );
+        return allAttributes;
     }
 
     public static Attribute remove(Attribute attribute) {
-        try {
-            return ABCommon.remove(Attribute.class, attribute.getName());
-        } catch (CannotFindABCoreException e) {
-            log(Level.SEVERE, e.getMessage());
-        }
-        return null;
+        return ATTRIBUTE_MAP.get(Attribute.class).remove(attribute.getName());
     }
 
     @SuppressWarnings("unchecked")
     public static <T> Effect<T> remove(Effect<T> effect) {
-        try {
-            return ABCommon.remove(Effect.class, effect.getName());
-        } catch (CannotFindABCoreException e) {
-            log(Level.SEVERE, e.getMessage());
-        }
-        return null;
+        return (Effect<T>) ATTRIBUTE_MAP.get(Effect.class).remove(effect.getName());
     }
 
     public static Attribute attributeOf(String name) {
-        try {
-            return ABCommon.get(Attribute.class, name);
-        } catch (CannotFindABCoreException e) {
-            log(Level.SEVERE, e.getMessage());
-        }
-        return null;
+        return ATTRIBUTE_MAP.get(Attribute.class).get(name);
     }
 
     @SuppressWarnings("unchecked")
     public static <T> Effect<T> effectOf(String name) {
-        try {
-            return ABCommon.get(Effect.class, name);
-        } catch (CannotFindABCoreException e) {
-            log(Level.SEVERE, e.getMessage());
-        }
-        return null;
+        return (Effect<T>) ATTRIBUTE_MAP.get(Effect.class).get(name);
     }
 }
