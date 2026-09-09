128a19a5ab8f38aaf68d4e8cfb0cdad5ef244b86
BuildTools <unconfigured@null.spigotmc.org>
2026-09-09 14:55:05 +0100

    updating version

--- src/main/java/com/github/oobila/bukkit/effects/PotionEffectType.java
+++ src/main/java/com/github/oobila/bukkit/effects/PotionEffectType.java
@@ -6,16 +6,16 @@ import lombok.Getter;
 public enum PotionEffectType {
 
     SPEED(org.bukkit.potion.PotionEffectType.SPEED),
-    SLOW(org.bukkit.potion.PotionEffectType.SLOW),
-    FAST_DIGGING(org.bukkit.potion.PotionEffectType.FAST_DIGGING),
-    SLOW_DIGGING(org.bukkit.potion.PotionEffectType.SLOW_DIGGING),
-    INCREASE_DAMAGE(org.bukkit.potion.PotionEffectType.INCREASE_DAMAGE),
-    HEAL(org.bukkit.potion.PotionEffectType.HEAL),
-    HARM(org.bukkit.potion.PotionEffectType.HARM),
-    JUMP(org.bukkit.potion.PotionEffectType.JUMP),
-    CONFUSION(org.bukkit.potion.PotionEffectType.CONFUSION),
+    SLOWNESS(org.bukkit.potion.PotionEffectType.SLOWNESS),
+    HASTE(org.bukkit.potion.PotionEffectType.HASTE),
+    MINING_FATIGUE(org.bukkit.potion.PotionEffectType.MINING_FATIGUE),
+    STRENGTH(org.bukkit.potion.PotionEffectType.STRENGTH),
+    INSTANT_HEALTH(org.bukkit.potion.PotionEffectType.INSTANT_HEALTH),
+    INSTANT_DAMAGE(org.bukkit.potion.PotionEffectType.INSTANT_DAMAGE),
+    JUMP_BOOST(org.bukkit.potion.PotionEffectType.JUMP_BOOST),
+    NAUSEA(org.bukkit.potion.PotionEffectType.NAUSEA),
     REGENERATION(org.bukkit.potion.PotionEffectType.REGENERATION),
-    DAMAGE_RESISTANCE(org.bukkit.potion.PotionEffectType.DAMAGE_RESISTANCE),
+    RESISTANCE(org.bukkit.potion.PotionEffectType.RESISTANCE),
     FIRE_RESISTANCE(org.bukkit.potion.PotionEffectType.FIRE_RESISTANCE),
     WATER_BREATHING(org.bukkit.potion.PotionEffectType.WATER_BREATHING),
     INVISIBILITY(org.bukkit.potion.PotionEffectType.INVISIBILITY),
@@ -36,7 +36,15 @@ public enum PotionEffectType {
     CONDUIT_POWER(org.bukkit.potion.PotionEffectType.CONDUIT_POWER),
     DOLPHINS_GRACE(org.bukkit.potion.PotionEffectType.DOLPHINS_GRACE),
     BAD_OMEN(org.bukkit.potion.PotionEffectType.BAD_OMEN),
-    HERO_OF_THE_VILLAGE(org.bukkit.potion.PotionEffectType.HERO_OF_THE_VILLAGE);
+    HERO_OF_THE_VILLAGE(org.bukkit.potion.PotionEffectType.HERO_OF_THE_VILLAGE),
+    DARKNESS(org.bukkit.potion.PotionEffectType.DARKNESS),
+    TRIAL_OMEN(org.bukkit.potion.PotionEffectType.TRIAL_OMEN),
+    RAID_OMEN(org.bukkit.potion.PotionEffectType.RAID_OMEN),
+    WIND_CHARGED(org.bukkit.potion.PotionEffectType.WIND_CHARGED),
+    WEAVING(org.bukkit.potion.PotionEffectType.WEAVING),
+    OOZING(org.bukkit.potion.PotionEffectType.OOZING),
+    INFESTED(org.bukkit.potion.PotionEffectType.INFESTED),
+    BREATH_OF_THE_NAUTILUS(org.bukkit.potion.PotionEffectType.BREATH_OF_THE_NAUTILUS);
 
     private final org.bukkit.potion.PotionEffectType type;
 
